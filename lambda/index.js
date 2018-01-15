d9c5d431904bc9b9ee1e957376c6d48d1008a2d1
Hendrik Poernama <poernahi@gmail.com>
2018-01-15 14:52:59 +0700

    Auto rotate image

--- lambda/index.js
+++ lambda/index.js
@@ -14,41 +14,45 @@ const ORIGINAL_PATH = process.env.ORIGINAL_PATH;
 const IMAGE_SIZES = {
   'medium': [180, 240],
   'large': [450, 600],
-  'default': [768, 1024]
+  'default': [840, 1120]
 };
 
 exports.handler = function(event, context, callback) {
   const test = event.queryStringParameters.test;
   const key = event.queryStringParameters.key;
   const match = key.match(/images\/((.*)_([^_]+)|[^_]+).jpg/);
-  //const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  //const width = parseInt(match[1], 10);
-  //const height = parseInt(match[2], 10);
-  //const originalKey = match[3];
   const baseKey  = match[2] || match[1];
   const modifier = match[3] || 'default';
   const width  = IMAGE_SIZES[modifier][0];
   const height = IMAGE_SIZES[modifier][1];
 
   S3.getObject({Bucket: BUCKET, Key: ORIGINAL_PATH + baseKey + ".jpg"}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('jpeg')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        CacheControl: 'max-age=86400',
-        ContentType: 'image/jpeg',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': test ? `${TEST_URL}/${key}` : `${URL}/${key}?resized=true`},
-        body: '',
-      })
-    )
+    .then(function(data) {
+      console.log(`Fetched ${baseKey} from S3`);
+      const image = Sharp(data.Body);
+      image
+        .metadata()
+        .then(function(metadata) {
+          console.log('Metadata: height=' + metadata.height + ', width=' + metadata.width);
+          return image
+            .rotate(metadata.height >= metadata.width ? 0 : 90)
+            .resize(width, height)
+            .toFormat('jpeg')
+            .toBuffer();
+        })
+        .then(buffer => S3.putObject({
+          Body: buffer,
+          Bucket: BUCKET,
+          CacheControl: 'max-age=86400',
+          ContentType: 'image/jpeg',
+          Key: key,
+        }).promise())
+        .then(() => callback(null, {
+          statusCode: '301',
+          headers: {'location': test ? `${TEST_URL}/${key}` : `${URL}/${key}?resized=true`},
+          body: '',
+        }))
+        .catch(err => callback(err))
+    })
     .catch(err => callback(err))
 }
